88c1d25bdbb2728cac43d3b57a4f39d4377faa31
Cess <cessmbuguar@gmail.com>
2016-10-23 23:17:16 +0300

    specifying maximum length for reg and phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   validates :f_name, presence:true, length: { minimum:4  }, format: { with: NAME }  
   validates :l_name, presence:true, length: { minimum:4  }, format: { with: NAME }
   validates :email, presence:true, length: { minimum:30 }, format: { with: REGEX }, uniqueness:true
-  validates :reg, presence:true, length: 13, uniqueness:true
-  validates :phone, presence:true, length:10, numeric:true, uniqueness:true
+  validates :reg, presence:true, length: { maximum:14 }, uniqueness:true
+  validates :phone, presence:true, length: { maximum: 13}, numericality:true, uniqueness:true
 end
