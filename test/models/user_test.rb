6be0e6a54330bca34588dda8e3ac8d8c3fc29d18
Cess <cessmbuguar@gmail.com>
2016-10-23 22:42:13 +0300

    validity test for lname and fname presence and length

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(reg:"CI/00050/2013", f_name: "Felix", l_name:"Muik", email: "felix@gmail.com", phone: "0720778634" )
+  end
+  test "f_name and l_name should have minimum length" do
+    @user.f_name = "a" * 2 || @user.l_name = "a" * 2
+    assert_not  @user.valid?
+
+  end
+  test "l_name and f_name should not be empty" do
+    @user.f_name = " "  || @user.l_name = " " 
+    assert_not  @user.valid?
+  end
 end
