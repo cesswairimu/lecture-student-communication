64f7b781b496883245ed2b02dae1c1d2261aba76
Cess <cessmbuguar@gmail.com>
2016-10-26 22:21:20 +0300

    create action and permit strong params for the create input

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params_user)
+    if @user.save
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def params_user
+    params.require(:user).permit(:reg,:f_name,:l_name, :phone, :email, :password, :password_confirmation)
+  end
 end
