403aae0003a7ca52b9abb41fd91c3a57fd4cf245
Cess <cessmbuguar@gmail.com>
2016-10-26 23:46:25 +0300

    change maximum to minimum

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   NAME = /\A[a-zA-Z]+\z/
   validates :f_name, presence:true, length: { minimum:4  }, format: { with: NAME }  
   validates :l_name, presence:true, length: { minimum:4  }, format: { with: NAME }
-  validates :email, presence:true, length: { minimum:30 }, format: { with: REGEX }, uniqueness:true
+  validates :email, presence:true, length: { maximum:30 }, format: { with: REGEX }, uniqueness:true
   validates :reg, presence:true, length: { maximum:14 }, uniqueness:true
   validates :phone, presence:true, length: { maximum: 13}, numericality:true, uniqueness:true
   has_secure_password
