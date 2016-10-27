9e7333a4b5a19ad849b3af267fe9d9885869f147
Cess <cessmbuguar@gmail.com>
2016-10-27 22:47:37 +0300

    successful signup test

--- test/integration/signup_test.rb
+++ test/integration/signup_test.rb
@@ -7,9 +7,20 @@ class SignupTest < ActionDispatch::IntegrationTest
     get new_user_path
    assert_template 'users/new'
    assert_no_difference 'User.count' do
-    post_via_redirect users_path user: { reg: "6787788", f_name: "ces", l_name: "wai",
+    post users_path user: { reg: "6787788", f_name: "ces", l_name: "wai",
                                          password: "foobar", password_confirmation: "barfoo"}
    end
+   
  assert_template 'users/new'
    end
+  test "successful signup" do
+    get new_user_path
+    assert_difference 'User.count', 1 do
+      post users_path user: {reg: "ed/ooo1/908", f_name: "student", l_name: "student", 
+      email: "student@gmail.com", phone: "0720778637", password: "foobar",
+      password_confirmation: "foobar"}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
 end
