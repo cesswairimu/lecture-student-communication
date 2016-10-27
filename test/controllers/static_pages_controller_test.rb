ee354c2e12c833ffaa93265088911f29eef66763
Cess <cessmbuguar@gmail.com>
2016-10-27 23:20:59 +0300

    testing titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
+    assert_select "title", "Home | Lecturer Student Feedback"
   end
+  test "should get signup" do
+    get new_user_path
+    assert_response :success
+    assert_select "title", "Signup | Lecturer Student Feedback"
+  end
+
 
 end
