0655b58200babe867865f823405fe263e02211d4
Cess <cessmbuguar@gmail.com>
2016-10-27 21:53:30 +0300

    edit and update action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,17 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if  @user.update_attributes(params_user)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
   def params_user
