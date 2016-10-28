45f676cedcce1b27783a79873d3e4cbcd2fc8182
Cess <cessmbuguar@gmail.com>
2016-10-28 15:49:12 +0300

    sucessful edit test

--- test/integration/edit_user_test.rb
+++ test/integration/edit_user_test.rb
@@ -16,4 +16,16 @@ class EditUserTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
 
    end
+test "successful user edit" do
+get edit_user_path(@user)
+assert_template 'users/edit'
+patch user_path(@user), params: {
+user: {
+    reg: "ci/00050/203", f_name: "betty", l_name:"Cessy", phone:032425542, 
+            email:"betty@gmail.com",  password: "foobar", password_confirmation: "foobar" }
+      }
+assert_redirected_to @user
+assert_not flash.empty?
+end
+
 end
