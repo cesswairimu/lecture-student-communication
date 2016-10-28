f55738137c4043538f533de48cbb7b8847e3cb2c
Cess <cessmbuguar@gmail.com>
2016-10-28 15:33:26 +0300

    testing successful editing of a user

---
+++ test/integration/edit_user_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class EditUserTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:betty)
+  end
+  test "Unsuccessful  edits for user details" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+
+    patch user_path(@user),
+   params: { user: { reg: "ci/00050/203", f_name: "", l_name:"Cessy", phone:032425542, 
+             password: "foobar", password_confirmation: "barfoo" }
+    }
+   assert_not flash.empty?
+    assert_template 'users/edit'
+
+   end
+end
