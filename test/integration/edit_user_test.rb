090c9ac8b3e460f58a0fb4ecdd5585a60c45d6d0
Cess <cessmbuguar@gmail.com>
2016-10-30 21:28:25 +0300

    testing user edit

--- test/integration/edit_user_test.rb
+++ test/integration/edit_user_test.rb
@@ -16,16 +16,23 @@ class EditUserTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
 
    end
+
 test "successful user edit" do
 get edit_user_path(@user)
-assert_template 'users/edit'
-patch user_path(@user), params: {
-user: {
-    reg: "ci/00050/203", f_name: "betty", l_name:"Cessy", phone:032425542, 
-            email:"betty@gmail.com",  password: "foobar", password_confirmation: "foobar" }
-      }
-assert_redirected_to @user
+ #log_in_as(@user) 
+#assert_redirected_to edit_user_path(@user)
+l_name = "Foo Bar"
+email = "foo@bar.com"
+patch user_path(@user), user: {l_name:l_name,
+email: email,
+password:
+"foobar",
+password_confirmation: "foobar" }
 assert_not flash.empty?
-end
+assert_redirected_to @user
+@user.reload
+assert_equal @user.l_name, l_name
+assert_equal @user.email, email
 
+end
 end
