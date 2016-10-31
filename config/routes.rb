bc1c172bfee120c0ffa5d9828dc88cfa39486d1e
Cess <cessmbuguar@gmail.com>
2016-10-31 08:11:13 +0300

    adding standard RESTful routes  for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
 
 root 'static_pages#home'
+get 'login' => 'sessions#new'
+post 'login' => 'sessions#create'
+delete 'logout' => 'sessions#destroy'
 resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
