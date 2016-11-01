6e6901b9ed7e7b656b23bac15fe3e884bb26467a
Cess <cessmbuguar@gmail.com>
2016-11-01 14:32:07 +0300

    create for session creation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
+  def create
+    user = User.find_by(reg: params[:session][:reg])
+    if user && user.authenticated(params[:session][:password])
+    else
+      flash[:danger] = "There was an error with your login"
+      render 'new'
+    end
+  end
 end
