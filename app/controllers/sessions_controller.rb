23c31b7e6cd54541728d577d439359d5e7cc8cee
Cess <cessmbuguar@gmail.com>
2016-11-02 21:55:46 +0300

    loggin in a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(reg: params[:session][:reg])
     if user && user.authenticated(params[:session][:password])
+login_user
+redirect_to user
     else
       flash[:danger] = "There was an error with your login"
       render 'new'
