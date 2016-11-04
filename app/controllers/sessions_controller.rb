cf0b9076af91d3d72739be24eeab3436f953e3c1
Cess <cessmbuguar@gmail.com>
2016-11-04 22:02:51 +0300

    change login params from reg to email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,10 @@ class SessionsController < ApplicationController
   def new
   end
   def create
-    user = User.find_by(reg: params[:session][:reg])
-    if user && user.authenticated(params[:session][:password])
-login_user
-redirect_to user
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      login user
+      redirect_to user
     else
       flash[:danger] = "There was an error with your login"
       render 'new'
