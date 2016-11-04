483994672f68c101d2af9b1e7b9796369cdb8ae3
Cess <cessmbuguar@gmail.com>
2016-11-04 22:03:31 +0300

    change the method from login_user to login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,10 +1,11 @@
 module SessionsHelper
-  def login_user(user)
+  def login(user)
     session[:user_id]= user_id
   end
   def current
-    @current ||= User.find_by(session: [:user_id])
+    @current ||= User.find_by(id: session[:user_id])
   end
+  
   def logged_in?
     !current.nil?
   end
