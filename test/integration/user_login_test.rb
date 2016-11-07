8f926690984d2755a1c3a1bf14040a7669a7c8d1
Cess <cessmbuguar@gmail.com>
2016-11-08 00:30:27 +0300

    test for login a user with valid information

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -7,4 +7,14 @@ class UserLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:betty)
   end
+  test "login a user if information is valid" do
+get login_path
+post login_path, session: {email: @user.email, password:'password'  }
+assert_redirected_to @user
+follow_redirect!
+assert_template 'users/show'
+assert_select "a[href=?]", login_path, count: 0
+assert_select "a[href=?]", logout_path
+assert_select "a[href=?]", user_path(@user)
+  end
 end
