116ce9ab5d75aadc296495d6662e3bc04f7dd6aa
Cess <cessmbuguar@gmail.com>
2016-11-11 23:07:35 +0300

    remove the default comment

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,4 @@ post 'login' => 'sessions#create'
 delete 'logout' => 'sessions#destroy'
 resources :users
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
