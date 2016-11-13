26bbcafeb733ba36fa64d80a1f8bba51d08fc7f3
Cess <cessmbuguar@gmail.com>
2016-11-13 23:07:56 +0300

    test for unavailable remember_digest

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,5 +48,8 @@ end
     @user.password = "a" * 4 
     assert_not @user.valid?
   end
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 
 end
