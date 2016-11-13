479e8d627409163f789c4db5f99284099833f3fb
Cess <cessmbuguar@gmail.com>
2016-11-13 23:07:09 +0300

    handling a non_existent remember_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember__token))
   end
   def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
