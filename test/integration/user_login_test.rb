7f3c0fbfb9876f0e9a3a2d7c795641bc108eec78
Cess <cessmbuguar@gmail.com>
2016-11-14 23:46:36 +0300

    test for remember me checkbox

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -8,22 +8,30 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     @user = users(:betty)
   end
   test "login a user if information is valid" do
-get login_path
-post login_path, session: {email: @user.email, password:'password'  }
-assert_redirected_to @user
-follow_redirect!
-assert_template 'users/show'
-assert_select "a[href=?]", login_path, count: 0
-assert_select "a[href=?]", logout_path
-assert_select "a[href=?]", user_path(@user)
-delete logout_path
-assert_not logged_in?
-assert_redirected_to root_url
-delete logout_path
-assert_select "a[href=?]", login_path
-assert_select "a[href=?]", logout_path, count: 0
-assert_select "a[href=?]", user_path(@user), count: 0 
+    get login_path
+    post login_path, session: {email: @user.email, password:'password'  }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not logged_in?
+    assert_redirected_to root_url
+    delete logout_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0 
 
 
+  end
+  test "login with remembering" do
+    login_as(@user, remember_me: '1')
+    assert_not_nil cookies['remember_token']
+  end
+  test "login without remembering" do
+    login_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
   end
 end
