39a628e5d5ffadb26a437682cfc6bd6657dcc441
Cess <cessmbuguar@gmail.com>
2016-11-14 23:41:53 +0300

    add login_as helper method

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,5 +9,19 @@ class ActiveSupport::TestCase
   def logged_in?
     !session[:user_id].nil?
   end
-
+  def login_as(user, option={})
+    password = option[:password] || 'password'
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, session: { email: user.email, 
+                                  password: password,
+                                  remember_me: remember_me}
+    else
+      session[:user_id] = user.id
+    end
+  end
+  private
+  def integration_test?
+    defined?(post_via_redirect)
+  end
 end
