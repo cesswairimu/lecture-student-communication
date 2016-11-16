d2793988ea9b2c1c275b9734682399c881aafc83
Cess <cessmbuguar@gmail.com>
2016-11-16 16:59:07 +0300

    add activation_digest column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161111192850) do
+ActiveRecord::Schema.define(version: 20161116135819) do
 
   create_table "users", force: :cascade do |t|
     t.string   "reg"
@@ -18,10 +18,13 @@ ActiveRecord::Schema.define(version: 20161111192850) do
     t.string   "l_name"
     t.string   "email"
     t.integer  "phone"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.string   "activation_digest"
+    t.boolean  "activated"
+    t.datetime "activated_at"
   end
 
 end
