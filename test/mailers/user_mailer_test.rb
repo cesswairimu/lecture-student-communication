d359ef34eb1e0a44d70042f1b817a10f5bf981d2
Cess <cessmbuguar@gmail.com>
2016-11-19 22:47:36 +0300

    testing accout activation

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,10 +2,15 @@ require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
   test "account_activation" do
-    mail = UserMailer.account_activation
+    user = users(:betty)
+    user.activation_token = User.token
+    mail = UserMailer.account_activation(user)
     assert_equal "Account activation", mail.subject
-    assert_equal ["to@example.org"], mail.to
+    assert_equal [user.mail], mail.to
     assert_equal ["from@example.com"], mail.from
+    assert_match user.name, mail.body.encoded
+    assert_match user.activation_token, mail.body.encoded
+    assert_match CGI::escape(user.email), mail.body.encoded
     assert_match "Hi", mail.body.encoded
   end
 
