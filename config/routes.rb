3411575d94ebcaf049f2be18a7d8d9b6c1d8321d
Cess <cessmbuguar@gmail.com>
2016-12-18 19:00:21 +0300

    override edit path

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 
 root 'static_pages#home'
+get 'edit' => 'users#edit'
 get 'login' => 'sessions#new'
 post 'login' => 'sessions#create'
 delete 'logout' => 'sessions#destroy'
