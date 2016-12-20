dce33fff44035826091098affa89ffc3cda53278
Cess <cessmbuguar@gmail.com>
2016-12-20 19:12:13 +0300

    restore REST

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
 
 root 'static_pages#home'
-get 'edit' => 'users#edit'
 get 'login' => 'sessions#new'
 post 'login' => 'sessions#create'
 delete 'logout' => 'sessions#destroy'
